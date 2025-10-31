551e6963d924e1821146bb043a8f460f8f5d2400
ali.baqar <arbabmemon235@gmail.com>
2025-10-31 08:47:17 -0700

    cardui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world! faixan
-    </h1>
-    </div>
+   <div>
+  
+    
+    <Card/>
+  </div>
+  
+    
   );
 }
 
