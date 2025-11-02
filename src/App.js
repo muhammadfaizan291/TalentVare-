ccc55d836a51726957dcf159b1e8d2bfddda0443
Asad <muhammadfaizan2910000@gmail.com>
2025-11-03 03:52:07 +0500

    app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import JobCard from './components/JobCard';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#F4F4F4]">
+    <div className="max-w-[1443px] bg-[#F4F4F4]">
       
       <Header /> 
 
